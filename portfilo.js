a08a47d5a77687e25b94eaa1e269b2c2cc5833a2
karinamota2220 <75580845+karinamota2220@users.noreply.github.com>
2025-01-14 15:06:27 -0800

    Initial commit

--- portfilo.js
+++ portfilo.js
@@ -18,3 +18,29 @@ function changeImage(imageSrc) {
     const mainImage = document.getElementById('mainImage');
     mainImage.src = images[currentIndex];
 }
+
+// image section
+// Function to open the modal with the clicked image
+function openModal(img) {
+    var modal = document.getElementById("imageModal");
+    var modalImg = document.getElementById("modalImg");
+    var caption = document.getElementById("caption");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    caption.innerHTML = img.alt;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("imageModal");
+    modal.style.display = "none";
+}
+
+// Attach event listeners to image items for modal view
+document.querySelectorAll(".image-item img").forEach(function (img) {
+    img.addEventListener("click", function () {
+        openModal(this);
+    });
+});
+
